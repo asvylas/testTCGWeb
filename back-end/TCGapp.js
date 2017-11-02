d4f8d12ad5d7458d53a6e8fce757b2f5e7a540ef
asvylas <32981972+asvylas@users.noreply.github.com>
2017-11-02 18:39:37 +0200

    Changed stuff around
    To use github website viewer. ~

--- back-end/TCGapp.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const port = 3000;
-const server = http.createServer(function(req,res){
-  res.writeHead(200, {"Content-Type": "text/html"})
-  res.end("html")
-});
-
-server.listen(port, "127.0.0.1");
-console.log("Listening to port: " + port);
