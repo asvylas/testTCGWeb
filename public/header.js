19787225721577c15835b5e06fe1b2ed3d19762c
Andrius Svylas <32981972+asvylas@users.noreply.github.com>
2017-11-21 17:25:53 +0200

    aaaa
    aaaa

--- public/header.js
+++ public/header.js
@@ -6,6 +6,7 @@ const navList = document.querySelectorAll("div[data-nav]")
 const cardTypes = document.getElementById("ctypes")
 
 ctypes.addEventListener("click", function(e){
+  const createdDivs = document.querySelectorAll('div .map-hidden')
   const navToShow = document.querySelector(`div[data-nav="${e.target.id}"]`);
   cardTypes.className = "hidden";
   for (let i = 0; i < cardList.length; i++) {
@@ -14,13 +15,19 @@ ctypes.addEventListener("click", function(e){
   for (let i = 0; i < navList.length; i++) {
     navList[i].className = "hidden"
   }
+   for (let i = 0; i < createdDivs.length; i++) {
+    createdDivs[i].className = "map-hidden"
+  }
+  
+  
     navToShow.className = e.target.id+"container"
     if (e.target.id+"container" === "cardscontainer") {
       cardTypes.className = "row";
       for (let i = 0; i < cardList.length; i++) {
         cardList[i].className = "images"
       }
-    }
+}
+console.log(createdDivs)
 });
 
 const mapContainer = document.getElementById("map-container");
@@ -28,7 +35,7 @@ const createCardContainers = function(data){
   for (let i = 0; i < data.length; i++) {
     let div = document.createElement('div');
     div.innerHTML = "";
-    div.className= "images";
+    div.className= "map-hidden";
     mapContainer.appendChild(div);
   }
 };
